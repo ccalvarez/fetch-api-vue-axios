3a4ce71c4411a3735441aa6900f35e76e35cb9b0
Carolina Cordero <ccalvarez@gmail.com>
2017-05-22 17:33:36 -0600

    Fetching mock data

---
+++ app.js
@@ -0,0 +1,11 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {title: 'the very first post', abstract: 'Lorem ipsum dolor sit amet.'},
+      {title: 'and then there was the second', abstract: 'Lorem ipsum dolor sit amet.'},
+      {title: 'third time\'s a charm', abstract: 'Lorem ipsum dolor sit amet.'},
+      {title: 'four the last time', abstract: 'Lorem ipsum dolor sit amet.'}
+    ]
+  }
+});
