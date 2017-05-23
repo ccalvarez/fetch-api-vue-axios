b34533f95d60010c7feacd41a33ab371b3258a4d
Carolina Cordero <ccalvarez@gmail.com>
2017-05-22 18:09:24 -0600

    Fetching data from NYTimes Top Stories API

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      {title: 'the very first post', abstract: 'Lorem ipsum dolor sit amet.'},
-      {title: 'and then there was the second', abstract: 'Lorem ipsum dolor sit amet.'},
-      {title: 'third time\'s a charm', abstract: 'Lorem ipsum dolor sit amet.'},
-      {title: 'four the last time', abstract: 'Lorem ipsum dolor sit amet.'}
-    ]
+    results: []
+  },
+  mounted() {
+    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=your-api-key")
+    .then(response => {this.results = response.data.results})
   }
 });
