28d9aff95627941626cca74efc986827bb1cbdc2
Carolina Cordero <ccalvarez@gmail.com>
2017-05-23 10:56:00 -0600

    Added vue computed property processedPosts

--- app.js
+++ app.js
@@ -20,6 +20,26 @@ const vm = new Vue({
         this.results = response.data.results;
       }).catch(error => { console.log(error); });
     }
+  },
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      // Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      console.log(posts);
+
+      // Put Array into Chunks
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i, i+chunk);
+      }
+      return chunkedArray;
+    }
   }
 });
 
