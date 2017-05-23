dd2fbfb7af6b5cbde1be7d1da840cada1fd566d4
Carolina Cordero <ccalvarez@gmail.com>
2017-05-23 09:28:40 -0600

    Added method getPosts() to vue application

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = "your-api-key";
+
+function buildUrl (url) {
+  return NYTBaseUrl + url + ".json?api-key=" + ApiKey;
+}
+
 const vm = new Vue({
   el: '#app',
   data: {
     results: []
   },
   mounted() {
-    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=your-api-key")
-    .then(response => {this.results = response.data.results})
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      axios.get(url).then(response => {
+        this.results = response.data.results;
+      }).catch(error => { console.log(error); });
+    }
   }
 });
+
