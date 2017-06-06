70c0e4bac62b679c6745418fb393e654d2f0ec06
Carolina Cordero <ccalvarez@gmail.com>
2017-06-06 15:41:38 -0600

    Created the news-list Vue component!

--- app.js
+++ app.js
@@ -5,6 +5,45 @@ function buildUrl (url) {
   return NYTBaseUrl + url + ".json?api-key=" + ApiKey;
 }
 
+Vue.component('news-list', {
+  props: ['results'],
+  template: `
+    <section>
+      <div class="row" v-for="posts in processedPosts">
+        <div class="columns large-3 medium-6" v-for="post in posts">
+          <div class="card">
+          <div class="card-divider">
+          {{ post.title }}
+          </div>
+          <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+          <div class="card-section">
+            <p>{{ post.abstract }}</p>
+          </div>
+        </div>
+        </div>
+      </div>
+  </section>
+  `,
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      // Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      // Put Array into Chunks
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i, i+chunk);
+      }
+      return chunkedArray;
+    }
+  }
+});
+
 const vm = new Vue({
   el: '#app',
   data: {
@@ -19,26 +58,6 @@ const vm = new Vue({
       axios.get(url).then(response => {
         this.results = response.data.results;
       }).catch(error => { console.log(error); });
-    }
-  },
-  computed: {
-    processedPosts() {
-      let posts = this.results;
-
-      // Add image_url attribute
-      posts.map(post => {
-        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
-        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
-      });
-
-      console.log(posts);
-
-      // Put Array into Chunks
-      let i, j, chunkedArray = [], chunk = 4;
-      for (i=0, j=0; i < posts.length; i += chunk, j++) {
-        chunkedArray[j] = posts.slice(i, i+chunk);
-      }
-      return chunkedArray;
     }
   }
 });
